540118c30218799ce0e209f1bc8126169b3f5fd4
Daniel Angst <dcangst@gmail.com>
2021-01-29 10:44:45 +0100

    update figure nums

--- scripts/timeseries.R
+++ scripts/timeseries.R
@@ -130,21 +130,21 @@ plateLabelsT53[3] <- "c) mono B / A (40+)"
 plotAllPlatesSISensitive <- plotAllPlatesSI +
   facet_wrap(vars(plate), ncol = 2, nrow = 3, labeller = labeller(plate = plateLabelsT53))
 ggsave(
-  filename = file.path(outDir, "SI", "FigS5.pdf"),
+  filename = file.path(outDir, "SI", "FigS6.pdf"),
   plot = plotAllPlatesSISensitive %+% meanFrequencies[[1]],
   width = plotWidth, height = plotHeight, units = "mm"
 )
 
 # complete timeseries singleResistantCommunity
 ggsave(
-  filename = file.path(outDir, "SI", "FigS6.pdf"),
+  filename = file.path(outDir, "SI", "FigS7.pdf"),
   plot = plotAllPlatesSI %+% meanFrequencies[[3]],
   width = plotWidth, height = plotHeight, units = "mm"
 )
 
 # complete timeseries doubleResistantCommunity
 ggsave(
-  filename = file.path(outDir, "SI", "FigS7.pdf"),
+  filename = file.path(outDir, "SI", "FigS8.pdf"),
   plot = plotAllPlatesSI %+% meanFrequencies[[2]],
   width = plotWidth, height = plotHeight, units = "mm"
 )
